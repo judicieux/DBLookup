7920e3acb3272b2e17d2627a10681f0d69b4112a
g3zb0yy <b0d3ns33@criminal.li>
2021-04-04 02:38:00 +0200

    Create main.php

---
+++ main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Database Lookup</h1>
+	<img src="https://image.noelshack.com/fichiers/2021/13/7/1617495812-original.gif" />
+	<br /><br />
+	<form method="POST">
+		<input type="text" name="pseudo" />
+		<br />
+		<input type="submit" name="submit" />
+	</form>
+	<br />
+	<?php
+	if (!empty(isset($_POST["submit"])))
+	{
+		if (isset($_POST["pseudo"]))
+		{
+			$matches = array();
+			$pseudo = $_POST["pseudo"];
+			foreach (glob("databases/*") as $file) 
+			{
+				$handle = @fopen($file, "r");
+				if ($handle)
+				{
+				    while (!feof($handle))
+				    {
+				        $buffer = fgets($handle);
+				        if(strpos($buffer, $pseudo) !== FALSE)
+				            $matches[] = $buffer;
+				    }
+				    fclose($handle);
+				}
+			}
+			$result = array_unique($matches, SORT_REGULAR);
+			foreach($result as $key => $value) 
+			{
+				echo $key . $value;
+			}
+		}
+	}
+	?>
+</body>
+<style>
+	body
+	{
+		background-color:black;
+		color:red;
+		font-family: Arial;
+		text-align:center;
+	}
+</style>
+</html>
